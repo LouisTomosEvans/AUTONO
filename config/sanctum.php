261443fc63c455fc6d0441447ad6b9ad8e522eb3
Louis Tomos Evans <LouisTomosEvans@protonmail.com>
2022-05-26 09:07:39 +0100

    Update sanctum.php

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1, http://35.176.202.182',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1, http://35.176.202.182, app.autono.live',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
